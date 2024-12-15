8429086453c73e94880e1b0929b0d16fa3958f4a
Gurunath Busetti <guru.svu31@gmail.com>
2024-12-15 21:41:05 +0530

    mongo sharding

---
+++ src/main/java/com/guru/mongo_sharding/MongoShardingApplication.java
@@ -0,0 +1,13 @@
+package com.guru.mongo_sharding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoShardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoShardingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guru/mongo_sharding/MongoShardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.mongo_sharding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoShardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
